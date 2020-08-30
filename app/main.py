bb7772e4c99fb9306c0d6c7b5629b90c9c027dfa
Tina Shi <tshi@umail.ucsb.edu>
2020-08-30 04:25:03 -0700

    Added flask app to hash locations into seeds

---
+++ app/main.py
@@ -0,0 +1,92 @@
+from flask import Flask
+from flask import request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+	return "<h1>get stickbugged</h1>"
+
+google_API_key = ""
+
+def parse_args(value):
+	coords = value.split('&', 1)
+	lat = -1
+	lng = -1
+	for c in coords:
+		if c.split('=')[0] == "long":
+			lng = float(c.split('=')[1])
+		elif c.split('=')[0] == "lat":
+			lat = float(c.split('=')[1])
+	if lat == -1 or lng == -1:
+		return None
+	return (lat, lng)
+
+def get_geocode_coords(coordinates):
+	reverse_url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={coordinates[0]},{coordinates[1]}&key={google_API_key}"
+
+	response = requests.get(reverse_url)
+
+	print(reverse_url)
+	if response.json()["status"] == "OK":
+		result_address = response.json()["results"][0]["formatted_address"]
+
+		print(result_address)
+
+		result_address = result_address.replace(' ', '+')
+		geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={result_address}&key={google_API_key}"
+
+		response = requests.get(geocode_url)
+
+		result_location = response.json()["results"][0]["geometry"]["location"]
+
+		print(result_location)
+		return result_location
+	return None
+
+def hash_coords(coordinates):
+	if coordinates is None:
+		return -1
+	lat = lo = coordinates["lat"] + 90
+	lng = hi = coordinates["lng"] + 180
+	while lat < 0xffff and lng < 0xffff:
+		lo = lat
+		hi = lng
+		lat *= 10
+		lng *= 10
+	lo = int(lo)
+	hi = int(hi) << 16
+	return lo + hi
+
+@app.route("/seed")
+def hash():
+	args = request.args
+	# return f"<h1>{str(args)}</h1>"
+	seed = "git seed"
+	if args:
+		coords = (args["lat"], args["long"])
+		seed = -1
+		if coords:
+			new_coords = get_geocode_coords(coords)
+			seed = hash_coords(new_coords)
+		print(seed)
+		# return json.dumps({"seed": seed})
+		# return seed
+	return f"<h1>{seed}</h1>"
+
+# hash("long=-125.393308&lat=37.336796")
+
+# Radar stuff b/c radar doesn't like broke people who can't afford $500 tier
+# radar_API_key_public = "prj_test_pk_f4c86b3b990fb39500fd6771b35dda55f84a0b13"
+# radar_API_key_secret = ""
+
+# coordinates = (40.78382, -73.97536)
+
+# context_url = f"https://api.radar.io/v1/context?coordinates={coordinates[0]},{coordinates[1]}"
+
+# response = requests.get(context_url,
+# 	headers = {"Authorization": radar_API_key_public})
+
+# print(context_url)
+# print(response.json())
